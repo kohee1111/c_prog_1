ccad2330bca3be77361c017cb526cacbb2859960
kohee1111 <koheetg@gmail.com>
2026-01-10 17:48:10 +0600

    Create basic.c

---
+++ basic.c
@@ -0,0 +1,10 @@
+//converting form a chat to int (ASCII) 
+#include<stdio.h>
+#include<string.h>
+int main(void){
+    char a = 'a' ; 
+    int int_a = (int)a ; 
+    printf("The ascii value of char a is :%d\n"  , int_a) ;
+    printf("I am fucking back") ;
+    return 0 ;
+}
