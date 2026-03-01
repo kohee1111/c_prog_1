e60ee5b46928059a83956b9fa720b8271b271400
kohee <koheetg@gmail.com>
2026-03-01 16:11:03 +0000

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,1 +1,45 @@
+//all about files 
+_______________________________
+#include<stdio.h>
+#include<stdlib.h>
+int main(void){
+    FILE *fp = fopen("hello.c" , "r") ; 
+    if(fp == NULL){
+        perror("Failed to open file!") ; 
+            return -1 ;
+    }
+    int ch ; 
+    while((ch = fgetc(fp)) != EOF){
+        putchar(ch) ;
+    }
+    return 0 ;
+}
+________________________________
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+int main(void){
+    int fd = open("hello.c" , O_WRONLY | O_CREAT | O_TRUNC, 0x1A4) ; 
+    if(fd < 0){
+        return -1 ;
+    }
+    dprintf(fd , "Your number is :%d\n" , 100) ;
+    close(fd) ;
+    return 0 ;
+}
+_________________________________________
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+int main(void){
 
+    int fd = open("Hello.c" , O_CREAT | O_WRONLY | O_APPEND , 0x1A4) ; 
+    if(fd < 0){
+
+        return -1 ;
+    }
+    dprintf(fd , "Appending data to file!") ; 
+    close(fd) ; 
+    return 0  ;
+}
+_____________________________
